d3178df3ab26de6322c1fd75b77ee5281619f3a6
User <https://github.com/AtaiSDK/HomeWork.git>
2023-04-04 17:41:45 +0300

    Initial commit

---
+++ app/src/main/java/com/example/notes/MainFragment.java
@@ -0,0 +1,35 @@
+package com.example.notes;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class MainFragment extends Fragment {
+    Button button;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        button = view.findViewById(R.id.add_btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requireActivity().getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, new AddFragment())
+                        .commit();
+            }
+        });
+        return view;
+
+
+    }
+}
